75d3dda60a508ab7efb53ca3863493af428952d1
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-10 19:41:26 +0200

    Parse a HTML document in the tests

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ const {readFileSync} = require('fs');
 const {resolve} = require('path');
 const ord = require('ord');
 const tosource = require('tosource');
+const jsdom = require('jsdom');
 
 const specDirectory = resolve(__dirname,
   'node_modules/parametric-svg-spec/specs'
@@ -18,44 +19,50 @@ const specs = ['usage-html5.yaml']
     resolve(specDirectory, filename)
   )));
 
-specs.forEach(({name, tests}) => tests.forEach(({description, ast}) => {
+specs.forEach(({name, tests}) => tests.forEach((
+  {description, ast, document}
+) => {
   test(`${name}: ${description}`, (a) => {
-    const result = parse();
+    jsdom.env(document, (error, window) => {
+      if (error) throw error;
 
-    ast.forEach((expected, index) => {
-      const nth = ord(index + 1);
-      const actual = result[index];
+      const result = parse(window.document.body.parentNode);
 
-      a.same(
-        expected.address,
-        actual.address,
-        `The \`address\` matches in the ${nth} parametric element`
-      );
-
-      a.is(
-        expected.name,
-        actual.name,
-        `The \`name\` matches in the ${nth} parametric element`
-      );
-
-      a.same(
-        expected.dependencies,
-        actual.dependencies,
-        `The \`dependencies\` match in the ${nth} parametric element`
-      );
-
-      ast.relation.forEach(({input, expectedOutput}) => {
-        const inputArguments = input.map(tosource).join(', ');
+      ast.forEach((expected, index) => {
+        const nth = ord(index + 1);
+        const actual = result[index];
 
         a.same(
-          actual.relation(...input),
-          expectedOutput,
-          'The relation function gives the expected result given the ' +
-          `arguments \`(${inputArguments})\`.`
+          expected.address,
+          actual.address,
+          `The \`address\` matches in the ${nth} parametric element`
         );
-      });
-    });
 
-    a.end();
+        a.is(
+          expected.name,
+          actual.name,
+          `The \`name\` matches in the ${nth} parametric element`
+        );
+
+        a.same(
+          expected.dependencies,
+          actual.dependencies,
+          `The \`dependencies\` match in the ${nth} parametric element`
+        );
+
+        ast.relation.forEach(({input, expectedOutput}) => {
+          const inputArguments = input.map(tosource).join(', ');
+
+          a.same(
+            actual.relation(...input),
+            expectedOutput,
+            'The relation function gives the expected result given the ' +
+            `arguments \`(${inputArguments})\`.`
+          );
+        });
+      });
+
+      a.end();
+    });
   });
 }));
