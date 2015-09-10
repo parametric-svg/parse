3e58b91f74e96704720c01679c601afe70c4abb0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-10 22:50:57 +0200

    Crawl attributes

--- module/index.js
+++ module/index.js
@@ -1,3 +1,26 @@
 const ast = require('parametric-svg-ast');
+const arrayFrom = require('array-from');
 
-export default () => ast({attributes: [], defaults: []});
+const crawl = (parentAddress) => (attributes, element, indexInParent) => {
+  const address = parentAddress.concat(indexInParent);
+
+  const currentAttributes = arrayFrom(element.attributes)
+    .filter(({localName}) => localName.match(/^parametric:/))  // POC
+    .map(({localName, value}) => ({
+      address,
+      name: localName.replace(/^parametric:/, ''),  // POC
+      dependencies: [],
+      relation: new Function(`return ${value}`),  // Proof of concept
+    }));
+
+  return arrayFrom(element.children).reduce(
+    crawl(address),
+    attributes.concat(currentAttributes)
+  );
+};
+
+export default (root) => {
+  const attributes = arrayFrom(root.children).reduce(crawl([]), []);
+
+  return ast({attributes, defaults: []});
+};
