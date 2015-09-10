cf8540510bade1dd30a44769ca26b002fc76be50
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-10 18:58:58 +0200

    Only test against one spec for now

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 const test = require('ava');
 const {safeLoad: yaml} = require('js-yaml');
-const {readFileSync, readdirSync} = require('fs');
+// const {readFileSync, readdirSync} = require('fs');
+const {readFileSync} = require('fs');
 const {resolve} = require('path');
 
 const specDirectory = resolve(__dirname,
   'node_modules/parametric-svg-spec/specs'
 );
 
-const specs = readdirSync(specDirectory)
+// const specs = readdirSync(specDirectory)
+const specs = ['usage-html5.yaml']
   .map((filename) => yaml(readFileSync(
     resolve(specDirectory, filename)
   )));
