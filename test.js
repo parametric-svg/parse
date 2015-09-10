dbd14348808fffda442a4b77a0d5a1cd1b92d228
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-10 17:41:21 +0200

    Bootstrap ava tests

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-const test = require('tape-catch');
+const test = require('ava');
+const {safeLoad: yaml} = require('js-yaml');
+const {readFileSync} = require('fs');
+const {resolve} = require('path');
+
+const specs = [
+  {name: 'Syntax', file: 'syntax.yaml'},
+].map(({name, file}) => ({
+  name,
+  spec: yaml(readFileSync(
+    resolve(__dirname, `node_modules/parametric-svg-spec/specs/${file}`)
+  )),
+}));
+
+specs.forEach(({name, spec}) => {
+  spec.tests.forEach(({description}) => {
+    test(`${name}: ${description}`, (a) => {
+      a.fail();
+      a.end();
+    });
+  });
+});
