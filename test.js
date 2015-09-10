558dc6ecc84c79781364d8bd4fa9241e86146e8a
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-10 19:34:16 +0200

    Deindent

--- test.js
+++ test.js
@@ -18,46 +18,44 @@ const specs = ['usage-html5.yaml']
     resolve(specDirectory, filename)
   )));
 
-specs.forEach(({name, tests}) => {
-  tests.forEach(({description, ast}) => {
-    test(`${name}: ${description}`, (a) => {
-      const result = parse();
+specs.forEach(({name, tests}) => tests.forEach(({description, ast}) => {
+  test(`${name}: ${description}`, (a) => {
+    const result = parse();
 
-      ast.forEach((expected, index) => {
-        const nth = ord(index + 1);
-        const actual = result[index];
+    ast.forEach((expected, index) => {
+      const nth = ord(index + 1);
+      const actual = result[index];
+
+      a.same(
+        expected.address,
+        actual.address,
+        `The \`address\` matches in the ${nth} parametric element`
+      );
+
+      a.is(
+        expected.name,
+        actual.name,
+        `The \`name\` matches in the ${nth} parametric element`
+      );
+
+      a.same(
+        expected.dependencies,
+        actual.dependencies,
+        `The \`dependencies\` match in the ${nth} parametric element`
+      );
+
+      ast.relation.forEach(({input, expectedOutput}) => {
+        const inputArguments = input.map(tosource).join(', ');
 
         a.same(
-          expected.address,
-          actual.address,
-          `The \`address\` matches in the ${nth} parametric element`
+          actual.relation(...input),
+          expectedOutput,
+          'The relation function gives the expected result given the ' +
+          `arguments \`(${inputArguments})\`.`
         );
-
-        a.is(
-          expected.name,
-          actual.name,
-          `The \`name\` matches in the ${nth} parametric element`
-        );
-
-        a.same(
-          expected.dependencies,
-          actual.dependencies,
-          `The \`dependencies\` match in the ${nth} parametric element`
-        );
-
-        ast.relation.forEach(({input, expectedOutput}) => {
-          const inputArguments = input.map(tosource).join(', ');
-
-          a.same(
-            actual.relation(...input),
-            expectedOutput,
-            'The relation function gives the expected result given the ' +
-            `arguments \`(${inputArguments})\`.`
-          );
-        });
       });
-
-      a.end();
     });
+
+    a.end();
   });
-});
+}));
