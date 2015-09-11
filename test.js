2d733eecb25e93b9f57c5f17506963c9ab390e54
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-11 14:17:37 +0200

    Prepare tests for running in browser

--- test.js
+++ test.js
@@ -2,78 +2,99 @@ import parse from './module';
 
 const test = require('tape-catch');
 const {safeLoad: yaml} = require('js-yaml');
-// const {readFileSync, readdirSync} = require('fs');
-const {readFileSync} = require('fs');
-const {resolve} = require('path');
 const ord = require('ord');
 const tosource = require('tosource');
 const {jsdom} = require('jsdom');
 const arrayFrom = require('array-from');
-const {DOMParser} = require('xmldom');
+const {DOMParser: XmldomParser} = require('xmldom');
 
-const specDirectory = resolve(__dirname,
-  'node_modules/parametric-svg-spec/specs'
-);
+if (typeof require.ensure !== 'function') require.ensure =
+  require('isomorphic-ensure')({
+    loaders: {
+      raw: require('raw-loader'),
+    },
+    dirname: __dirname,
+  });
 
-// const specs = readdirSync(specDirectory)
-const specs = ['usage-html5.yaml', 'usage-xml.yaml']
-  .map((filename) => yaml(readFileSync(
-    resolve(specDirectory, filename)
-  )));
+const specPaths = [
+  'usage-html5',
+  'usage-xml',
+].map((spec) => `raw!./node_modules/parametric-svg-spec/specs/${spec}.yaml`);
 
-specs.forEach(({name, tests}) => tests.forEach((
-  {description, ast, document, mode}
-) => {
-  test(`${name}: ${description}`, (is) => {
-    const rootElement = (mode === 'html' ?
-      jsdom(document).defaultView.document.body.parentNode :
-      new DOMParser().parseFromString(document).documentElement
-    );
+require.ensure(specPaths, (require) => {
+  const specs = specPaths.map((path) => yaml(require(path)));
 
-    const {attributes} = parse(rootElement);
-
-    is.equal(
-      attributes.size,
-      ast.length,
-      'the AST has the right number of attributes'
-    );
-
-    ast.forEach((expected, index) => {
-      const n = index + 1;
-      const nth = `${n}${ord(n)}`;
-      const actual = arrayFrom(attributes)[index];
-
-      is.deepEqual(
-        actual.address,
-        expected.address,
-        `the \`address\` matches in the ${nth} parametric attribute`
+  specs.forEach((
+    {name, tests}
+  ) => tests.forEach((
+    {description, ast, document, mode}
+  ) => {
+    test(`${name}: ${description}`, (is) => {
+      const inBrowser = typeof window !== 'undefined' && window.DOMParser;
+      const htmlMode = mode === 'html';
+      const rootElement = (
+        (inBrowser && htmlMode &&
+          (new window.DOMParser()).parseFromString(document, 'text/html')
+            .documentElement
+        ) ||
+        (inBrowser && !htmlMode &&
+          (new window.DOMParser()).parseFromString(document, 'application/xml')
+            .documentElement
+        ) ||
+        (!inBrowser && htmlMode &&
+          jsdom(document).defaultView.document.body.parentNode
+        ) ||
+        (!inBrowser && !htmlMode &&
+          (new XmldomParser()).parseFromString(document, 'application/xml')
+            .documentElement
+        )
       );
+
+      const {attributes} = parse(rootElement);
 
       is.equal(
-        actual.name,
-        expected.name,
-        `the \`name\` matches in the ${nth} parametric attribute`
+        attributes.size,
+        ast.length,
+        'the AST has the right number of attributes'
       );
 
-      is.deepEqual(
-        actual.dependencies,
-        expected.dependencies,
-        `the \`dependencies\` match in the ${nth} parametric attribute`
-      );
-
-      expected.relation.forEach(({input, output: expectedOutput}) => {
-        const inputArguments = input.map(tosource).join(', ');
+      ast.forEach((expected, index) => {
+        const n = index + 1;
+        const nth = `${n}${ord(n)}`;
+        const actual = arrayFrom(attributes)[index];
 
         is.deepEqual(
-          actual.relation(...input),
-          expectedOutput,
-          `the \`relation\` in the ${nth} parametric attribute returns ` +
-          'the expected value when called with the arguments ' +
-          `\`(${inputArguments})\`.`
+          actual.address,
+          expected.address,
+          `the \`address\` matches in the ${nth} parametric attribute`
         );
-      });
-    });
 
-    is.end();
-  });
-}));
+        is.equal(
+          actual.name,
+          expected.name,
+          `the \`name\` matches in the ${nth} parametric attribute`
+        );
+
+        is.deepEqual(
+          actual.dependencies,
+          expected.dependencies,
+          `the \`dependencies\` match in the ${nth} parametric attribute`
+        );
+
+        expected.relation.forEach(({input, output: expectedOutput}) => {
+          const inputArguments = input.map(tosource).join(', ');
+
+          is.deepEqual(
+            actual.relation(...input),
+            expectedOutput,
+            `the \`relation\` in the ${nth} parametric attribute returns ` +
+            'the expected value when called with the arguments ' +
+            `\`(${inputArguments})\`.`
+          );
+        });
+      });
+
+      is.end();
+    });
+  }));
+});
