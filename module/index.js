b831b7939856d31c4edd0f2562e202e7e722291c
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-15 19:12:03 +0200

    Take a shallow DOM structure

--- module/index.js
+++ module/index.js
@@ -23,7 +23,7 @@ const getLocalName = (node) => (node.namespaceURI ?
   node.name.replace(new RegExp(`^.*?:`), '')
 );
 
-const crawl = (parentAddress) => (attributes, element, indexInParent) => {
+const crawl = (parentAddress) => (allAttributes, element, indexInParent) => {
   const address = parentAddress.concat(indexInParent);
 
   const currentAttributes = arrayFrom(element.attributes)
@@ -41,12 +41,12 @@ const crawl = (parentAddress) => (attributes, element, indexInParent) => {
 
   return getChildren(element).reduce(
     crawl(address),
-    attributes.concat(currentAttributes)
+    allAttributes.concat(currentAttributes)
   );
 };
 
 export default (root) => {
-  const attributes = getChildren(root).reduce(crawl([]), []);
+  const attributes = crawl([])([], root, 0);
 
   return ast({attributes, defaults: []});
 };
